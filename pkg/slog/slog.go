a2e179b714021443e9a403bf9d1a272025e09af0
Nicola Asuni <info@tecnick.com>
2025-11-08 21:17:12 +0000

    add slog interface to abstract logger dependencies

---
+++ pkg/slog/slog.go
@@ -0,0 +1,32 @@
+/*
+Package slog provides a generic log/slog compatible interface for structured logging capabilities.
+*/
+package slog
+
+// Logger defines the minimal interface used by this package for structured logging.
+type Logger interface {
+	Debug(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
+	Error(msg string, args ...any)
+}
+
+// NewNop returns a no-op Logger.
+func NewNop() Logger {
+	return &NopLogger{}
+}
+
+// NopLogger is a no-op implementation of the Logger interface.
+type NopLogger struct{}
+
+// Debug is a no-op.
+func (*NopLogger) Debug(_ string, _ ...any) {}
+
+// Info is a no-op.
+func (*NopLogger) Info(_ string, _ ...any) {}
+
+// Warn is a no-op.
+func (*NopLogger) Warn(_ string, _ ...any) {}
+
+// Error is a no-op.
+func (*NopLogger) Error(_ string, _ ...any) {}
---
+++ pkg/slog/slog_test.go
@@ -0,0 +1,19 @@
+package slog
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_NewNop(t *testing.T) {
+	t.Parallel()
+
+	logger := NewNop()
+	require.NotNil(t, logger)
+
+	logger.Debug("debug message", "key1", "value1")
+	logger.Info("info message", "key2", "value2")
+	logger.Warn("warn message", "key3", "value3")
+	logger.Error("error message", "key4", "value4")
+}
