75685a29c5e00e661db3be5c00d177971fa1c73a
Nicola Asuni <info@tecnick.com>
2025-11-23 13:15:01 +0000

    Fix uidc.NewID64 to set the epoch to January 1st of the current decade's starting year

--- pkg/uidc/uidc.go
+++ pkg/uidc/uidc.go
@@ -18,16 +18,18 @@ import (
 	"github.com/tecnickcom/gogen/pkg/random"
 )
 
-// NewID64 generates and returns a new base-36-string-formatted 64 bit unique ID based on time (high 32 bit) and a random number (low 32 bit).
-// NOTE: the zero time is set to the 1st of January of 10 years ago.
+// NewID64 returns a 64-bit unique ID encoded as a base-36 string.
+// The upper 32 bits contain a timestamp and the lower 32 bits are random.
+// The timestamp epoch is January 1st of the current decade's starting year.
 func NewID64() string {
 	t := time.Now().UTC()
-	offset := time.Date(t.Year()-10, 1, 1, 0, 0, 0, 0, time.UTC).Unix() // [s] time starts 1st JAN 10 years ago
+	offset := time.Date(t.Year()-t.Year()%10, 1, 1, 0, 0, 0, 0, time.UTC).Unix()
 
 	return strconv.FormatUint((((uint64)(t.Unix()-offset))<<32)+(uint64)(random.New(nil).RandUint32()), 36)
 }
 
-// NewID128 generates and returns a new base-36-string-formatted 128 bit unique ID based on time (high 64 bit) and a random number (low 64 bit).
+// NewID128 returns a 128-bit unique identifier encoded as a base-36 string.
+// The high 64 bits are derived from the current time; the low 64 bits are random.
 func NewID128() string {
 	return strconv.FormatUint((uint64)(time.Now().UTC().UnixNano()), 36) + strconv.FormatUint(random.New(nil).RandUint64(), 36)
 }
