6ff640e9448f01bd75bef149f279baf09d66b4ad
Nicola Asuni <info@tecnick.com>
2026-03-10 08:22:20 +0000

    deprecate randomkey package

--- pkg/randkey/randkey.go
+++ pkg/randkey/randkey.go
@@ -1,6 +1,8 @@
 /*
 Package randkey provides utility functions to generate random uint64 keys in
 different formats.
+
+Deprecated: Please use github.com/tecnickcom/gogen/pkg/random instead.
 */
 package randkey
 
@@ -14,26 +16,36 @@ import (
 var rnd = random.New(nil) //nolint:gochecknoglobals
 
 // RandKey stores the random key.
+//
+// Deprecated: Please use random instead.
 type RandKey struct {
 	key uint64
 }
 
 // New generates a new uint64 random key.
+//
+// Deprecated: Please use random.RandUint64() instead.
 func New() *RandKey {
 	return &RandKey{key: rnd.RandUint64()}
 }
 
 // Key returns a uint64 key.
+//
+// Deprecated: Please use random.RandUint64() instead.
 func (sk *RandKey) Key() uint64 {
 	return sk.key
 }
 
 // String returns a variable-length string key.
+//
+// Deprecated: Please use random.RandString64() instead.
 func (sk *RandKey) String() string {
 	return strconv.FormatUint(sk.key, 36)
 }
 
 // Hex returns a fixed-length 16 digits hexadecimal string key.
+//
+// Deprecated: Please use random.RandHex64() instead.
 func (sk *RandKey) Hex() string {
 	s := strconv.FormatUint(sk.key, 16)
 
