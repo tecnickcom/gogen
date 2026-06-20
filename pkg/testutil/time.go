1a76086e36d1176f46fd762a342cbaba4dc5a0f0
Nicola Asuni <info@tecnick.com>
2026-06-20 11:11:11 +0100

    perf: testutil - compile time-replacement regexps once
    - Hoist the ReplaceDateTime and ReplaceUnixTimestamp regexps to package-level compiled vars instead of recompiling per call.

--- pkg/testutil/time.go
+++ pkg/testutil/time.go
@@ -4,16 +4,20 @@ import (
 	"regexp"
 )
 
+// Precompiled regular expressions for timestamp replacement (compiled once at package load).
+var (
+	regexDateTime      = regexp.MustCompile("([0-9]{4}\\-[0-9]{2}\\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}[^\"]*)")
+	regexUnixTimestamp = regexp.MustCompile("([0-9]{19})")
+)
+
 // ReplaceDateTime replaces RFC3339-like datetime substrings in src with repl.
 // It is useful for deterministic assertions on JSON responses with dynamic timestamps.
 func ReplaceDateTime(src, repl string) string {
-	re := regexp.MustCompile("([0-9]{4}\\-[0-9]{2}\\-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}[^\"]*)")
-	return re.ReplaceAllString(src, repl)
+	return regexDateTime.ReplaceAllString(src, repl)
 }
 
 // ReplaceUnixTimestamp replaces 19-digit Unix-nanosecond timestamps in src with repl.
 // It is useful for deterministic assertions on JSON responses with dynamic timestamps.
 func ReplaceUnixTimestamp(src, repl string) string {
-	re := regexp.MustCompile("([0-9]{19})")
-	return re.ReplaceAllString(src, repl)
+	return regexUnixTimestamp.ReplaceAllString(src, repl)
 }
